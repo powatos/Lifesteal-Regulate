b33eed93c49928039cb0cb90b9419899b25faa7b
Sreejith <exetra63@gmail.com>
2025-08-21 22:58:54 -0400

    PvP disable fix
    Projectile also disabled

--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 * Inconsistent culling in safe chunks debug rendering mode
 * Inconsistent transparency effects in safe chunks debug rendering mode
   * fix: stack colors? through multiple safe chunk renders
+* Inconsistent projectile visibility when deflected in safe chunk
--- src/main/java/net/powato/lifesteal/mixin/PlayerEntityMixin.java
+++ src/main/java/net/powato/lifesteal/mixin/PlayerEntityMixin.java
@@ -1,9 +1,12 @@
 package net.powato.lifesteal.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.powato.lifesteal.SafeChunks.SafeChunks;
@@ -11,6 +14,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
@@ -18,7 +22,9 @@ public class PlayerEntityMixin {
     @Inject(method = "attack", at = @At("HEAD"), cancellable = true)
     private void CancelAttack(Entity target, CallbackInfo ci){
 
-        // TODO: only cancel on players, not other entities
+        if (!(target instanceof PlayerEntity)){ return; } // returns if 'target' isn't a Player Entity
+
+
         PlayerEntity Player = (PlayerEntity) (Object) this;
 
         if (Player.getWorld().isClient) return; // only run on server
@@ -31,5 +37,24 @@ public class PlayerEntityMixin {
         if (state.isSafeChunk(chunkPos, dimension)){
             ci.cancel();
         }
+    }
+
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    private void onDamage(ServerWorld World, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir){
+        PlayerEntity target = (PlayerEntity) (Object) this;
+
+        if (source.getSource() instanceof ProjectileEntity && source.getAttacker() instanceof ServerPlayerEntity player){
+            if (player.getWorld().isClient) return; // only run on server
+
+            SafeChunks state = SafeChunks.getServerState(player.getServer());
+
+            ChunkPos chunkPos = new ChunkPos(target.getBlockPos());
+            RegistryKey<World> dimension = target.getWorld().getRegistryKey();
+
+            if (state.isSafeChunk(chunkPos, dimension)){
+                cir.setReturnValue(false);
+            }
+        }
+
     }
 }
